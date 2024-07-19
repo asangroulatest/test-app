1060e1cbc2ab6497ecb77b24ed816585f5d2d77c
asangroula <asangroula@hubspot.com>
2024-07-19 14:11:35 -0500

    focus test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState, useRef} from 'react';
+import {styled} from 'styled-components';
+
+const Wrapper = styled.div`
+width: 400px;
+height: 100px;
+`
 
 function App() {
+
+const [showInput, setShowInput] = useState(false);
+const ref = useRef(null);
+
+useEffect(() => {
+  if(ref.current){
+    ref.current.focus();
+  }
+
+},[showInput])
+console.log('show input', showInput)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Wrapper onClick={() => {console.log('clicked'); setShowInput(true)}}>
+          {showInput && <p ref={ref} contentEditable={true}>Who is this</p>}
+          {!showInput && <p>Not this</p>}
+        </Wrapper>
+
       </header>
     </div>
   );
