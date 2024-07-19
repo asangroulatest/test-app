a0b9fd5996ab7168c7e782125e7ccb0eb5d6257d
asangroula <asangroula@hubspot.com>
2024-07-19 14:21:52 -0500

    adds placeholder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ width: 400px;
 height: 100px;
 `
 
-const StyledParagraph = styled.p``
+const StyledParagraph = styled.p`
+  outline: none !important;
+  height: 100%;
+  word-break: break-word;
+  border: ${({ error }) => (error ? '1px solid red' : 'none')};
+  &:empty {
+    ::before {
+      content: '${({ placeholder }) => placeholder}';
+      opacity: 0.6;
+    }
+  }
+`;
 
 function App() {
 
@@ -28,7 +39,7 @@ console.log('show input', showInput)
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Wrapper onClick={() => {console.log('clicked'); setShowInput(true)}}>
-          {showInput && <StyledParagraph ref={ref} contentEditable={true}/>}
+          {showInput && <StyledParagraph ref={ref} contentEditable={true} placeholder="Write a message.."/>}
           {!showInput && <p>Not this empty input</p>}
         </Wrapper>
 
