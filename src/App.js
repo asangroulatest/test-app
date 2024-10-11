063f97646b856fab3c6651f14368bef9c40842b7
asangroula <asangroula@hubspot.com>
2024-10-11 13:51:50 -0500

    different fonts

--- src/App.js
+++ src/App.js
@@ -2,94 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState, useRef, useCallback } from 'react';
 import { styled } from 'styled-components';
-const FormControlWrapper = styled.div`
-  width: 100%;
-  flex-basis: 100%;
-`;
 
-const FormWrapper = ({ children }) => {
-  const renChildren = children({});
-  return <FormControlWrapper>
-    {renChildren}
-  </FormControlWrapper>
+ function generateFontStylesheet(fontFamily,url) {
+  console.log('called')
+  const fontStylesheet = document.createElement('style');
+  //const url = 'https://api-na1.hubspotqa.com/_hcms/googlefonts/fonts/Noto_Sans/200';
+  //const url = 'https://cdn1.hubspot.net/googlefonts/fonts/Noto_Sans/regular';
+  fontStylesheet.textContent = `@font-face {
+        font-family: ${fontFamily};
+        src: url("${url}.woff2") format("woff2"), url("${url}.woff") format("woff");
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+      }`
+      document.head.appendChild(fontStylesheet);
 }
-
-const Wrapper = styled.div`
-  border: 1px solid black;
-  display: flex;
-  align-items: center;
-  min-height: 95px;
-  width: 300px;
-  max-width: 300px;
-  position: relative;
-  background: #ffffff;
-  padding: 20px 24px;
-  border-radius: 60px;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  cursor: text;
-  font-size: 14px;
-`
-
-const StyledParagraph = styled.p`
-  outline: none !important;
-  height: 100%;
-  word-break: break-word;
-  border: ${({ error }) => (error ? '1px solid red' : 'none')};
-  &:empty {
-    ::before {
-      content: '${({ placeholder }) => placeholder}';
-      opacity: 0.6;
-    }
-  };
-  font-size: 16px;
-  /* &:focus {
-        font-size: 14px !important;
-        border: 1px solid green;
-    } */
-`;
-
-const InputWrapper = styled.div`
-  width: 100%;
-  align-self: stretch;
-  font-size: 14px;
-`;
-
-const IntialMessageTextWrapper = styled.div`
-  width: 100%;
-`;
 
 function App() {
 
-  const [showInput, setShowInput] = useState(false);
-  const [inputMessage, setInputMessage] = useState('');
-  let inputElementRef = useRef(null);
-
-
-  const handleChange = (e) => {
-    const { innerText } = e.target;
-    setInputMessage(innerText);
-  };
+  useEffect(() => {
+generateFontStylesheet('MontSerrat','https://cdn1.hubspot.net/googlefonts/fonts/Montserrat/regular' );
+ generateFontStylesheet('Noto_Sans','https://api-na1.hubspotqa.com/_hcms/googlefonts/Noto_Sans/regular' );
+ generateFontStylesheet('Noto_Sans','https://api.hubapi.com/_hcms/googlefonts/Noto_Sans/regular' );
+  },[])
 
 
 
-
-  const callbackRef = useCallback(inputElement => {
-    if (inputElement) {
-      //inputElement.style.fontSize = '30px';
-      inputElementRef.current = inputElement;
-      inputElement.focus();
-    }
-  }, []);
-
-  console.log('renders show', showInput);
-
-
-  const tooLong = inputMessage.length > 7;
 
   return (
     <div className="App">
-     <h1>Welcome to my App!</h1>
+     <h2 >Default Font!!</h2>
+     <h2 style={{fontFamily: 'Montserrat'}}>Montserrat!!</h2>
+     <h2 style={{fontFamily: 'Noto_Sans'}}>Noto Sans!!</h2>
+
     </div>
   );
 }
